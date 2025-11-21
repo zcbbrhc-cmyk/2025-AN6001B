432fef7180178934286c60330403db6c3bbf646a
zcbbrhc-cmyk <zcbbrhc@gmail.com>
2025-11-21 07:34:12 +0000

    1

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask,render_template,request
+import joblib
 app= Flask(__name__)
 
 @app.route('/',methods=['GET','POST'])
@@ -11,5 +12,15 @@ def main():
     print(q)
     return(render_template('main.html'))
 
+@app.route('/dbs',methods=['GET','POST'])
+def dbs():
+    return(render_template('dbs.html'))
+
+@app.route('/dbs_prediction',methods=['GET','POST'])
+def dbs_prediction():
+    q=float(request.form.get("q"))
+    model=joblib.load('dbs.pkl')
+    r=model.predict([[q]])
+    return(render_template('dbs_prediciton.html',r=r[0][0]))
 if __name__=='__main__':
     app.run()
