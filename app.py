15eecaa0b93450f391533431acff7d693fa2e555
zcbbrhc-cmyk <zcbbrhc@gmail.com>
2025-11-21 06:32:11 +0000

    1

--- app.py
+++ app.py
@@ -1,8 +1,15 @@
 from flask import Flask,render_template,request
 app= Flask(__name__)
+
 @app.route('/',methods=['GET','POST'])
 def index():
     return(render_template('index.html'))
+
+@app.route('/main',methods=['GET','POST'])
+def main():
+    q=request.form.get("q")
+    print(q)
+    return(render_template('main.html'))
 
 if __name__=='__main__':
     app.run()
